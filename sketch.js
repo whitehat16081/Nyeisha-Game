7b08f963f6b88c4cd7be6f75c632859956c7c0f9
whitehat16081 <72308381+whitehat16081@users.noreply.github.com>
2021-04-29 14:03:40 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,13 +1,13 @@
 var backgroundImg;
 var king,kingImg,princess,princessImg,wolf,wolfImg;
-var gameState=2;
+var gameState=1;
 
 function preload(){
 backgroundImg=loadImage("Images/Bg3.jpg")
 kingImg=loadImage("Images/King.png")
 princessImg=loadImage("Images/Princess.png")
-wolfImg=loadImage("Images/wolf.png");
-winImg=loadImage("Images/win.png")
+wolfImg=loadImage("Images/wolf1.png");
+
 }
 
 
@@ -16,20 +16,22 @@ createCanvas(1350,800);
  king=createSprite(1200,700);
 king.addImage(kingImg);
 king.scale=0.4;
-king.debug=true;
+//king.debug=true;
 king.setCollider("circle",0,0,200);
 
 princess=createSprite(75,165);
 princess.addImage(princessImg);
 princess.scale=0.20;
-princess.debug=true;
+//princess.debug=true;
 princess.setCollider("circle",0,0,200); 
 
- /*win=createSprite(700,400,50,50);
- win.addImage(winImg);
- win.scale=0.4;
- win.visible=false;*/
+wolf=createSprite(570,340,50,50);
+wolf.addImage(wolfImg);
+wolf.scale=0.4;
 
+wolf1=createSprite(1060,430,50,50);
+wolf1.addImage(wolfImg);
+wolf1.scale=0.4;
 
 }
 function draw(){
@@ -48,6 +50,10 @@ if(keyDown(RIGHT_ARROW)){
 }
 if(keyDown(LEFT_ARROW)){
     princess.x=princess.x-20;
+}
+if(princess.isTouching(wolf)||princess.isTouching(wolf1)){
+    princess.x=75;
+    princess.y=175;
 }
 if(gameState===1){
     textSize(50);
@@ -78,7 +84,7 @@ else if(gameState===2){
     stroke("white");
     fill("white");
     text("Level 2",550,100);
-    drawWalls1();
+    
     drawWalls2();
     if(princess.isTouching(king)){
         textSize(50);
@@ -102,6 +108,7 @@ else if(gameState===3){
     stroke("white");
     fill("white");
     text("Level 3",550,100);
+    
     drawWalls3();
     if(princess.isTouching(king)){
         textSize(50);
@@ -124,6 +131,7 @@ else if(gameState===4){
     stroke("white");
     fill("white");
     text("Level 4",550,100)
+    
     drawWalls4();
     if(princess.isTouching(king)){
         textSize(50);
@@ -136,7 +144,6 @@ else if(gameState===4){
     }
 }
 
-bouncing();
 
 }
 function drawWalls1(){
@@ -160,7 +167,7 @@ function drawWalls1(){
    wall7.shapeColor="rgb(170,190,60)";
    wall8 = createSprite(500, 340,10,270);
    wall8.shapeColor="rgb(170,190,60)";
-   wall9 = createSprite(700, 555,10,350);
+   wall9 = createSprite(700, 525,10,280);
    wall9.shapeColor="rgb(170,190,60)";
    wall10 = createSprite(300, 450,10,200);
    wall10.shapeColor="rgb(170,190,60)";
@@ -189,51 +196,100 @@ function drawWalls1(){
 function drawWalls2() {
    
    wall13 = createSprite(820, 120,300,10);
-   wall13.shapeColor="yellow";
-   wall14 = createSprite(500, 500,225,10);
-   wall14.shapeColor="yellow";
-   wall15 = createSprite(610, 545,10,100);
-   wall15.shapeColor="yellow";
+   wall13.shapeColor="rgb(170,190,60)";
+   //wall14 = createSprite(500, 500,225,10);
+  // wall14.shapeColor="red";
+   wall15 = createSprite(410, 400,120,10);
+   wall15.shapeColor="rgb(170,190,60)";
    wall16 = createSprite(865, 730,100,10);
-   wall16.shapeColor="yellow";
-   wall17 = createSprite(900,520 ,120,10);
-   wall17.shapeColor="red";
+   wall16.shapeColor="rgb(170,190,60)";
+   wall17 = createSprite(1000,520 ,80,10);
+   wall17.shapeColor="rgb(170,190,60)";
   // wall18 = createSprite(640, 400,120,10);
   // wall18.shapeColor="yellow";
-
+    princess.collide(wall);
+    princess.collide(wall1);
+    princess.collide(wall2);
+    princess.collide(wall3);
+    princess.collide(wall4);
+    princess.collide(wall5);
+    princess.collide(wall6);
+    princess.collide(wall7);
+    princess.collide(wall8);
+    princess.collide(wall9);
+    princess.collide(wall10);
+    princess.collide(wall11);
+    princess.collide(wall12);
    princess.collide(wall13);
-   princess.collide(wall14);
+  
    princess.collide(wall15);
    princess.collide(wall16);
    princess.collide(wall17);
-   //princess.collide(wall18);
 }
 
 function drawWalls3(){
-   wall19 = createSprite(400,275,100,10);
-   wall19.shapeColor="red";
-   wall20 = createSprite(300,250,100,10);
-   wall20.shapeColor="blue";
-   wall21 = createSprite(680,400,100,10);
-   wall21.shapeColor="black";
-   wall22 = createSprite(250,400,100,10);
-   wall22.shapeColor="orange";
-   wall23 = createSprite(950,600,150,10);
-   wall23.shapeColor="pink";
+   wall18 = createSprite(700,380,120,10);
+   wall18.shapeColor="rgb(170,190,60)";
+   wall19 = createSprite(450,630,10,100);
+   wall19.shapeColor="rgb(170,190,60)";
+   wall20 = createSprite(1000,200,100,10);
+   wall20.shapeColor="rgb(170,190,60)";
+   
+   princess.collide(wall);
+   princess.collide(wall1);
+   princess.collide(wall2);
+   princess.collide(wall3);
+   princess.collide(wall4);
+   princess.collide(wall5);
+   princess.collide(wall6);
+   princess.collide(wall7);
+   princess.collide(wall8);
+   princess.collide(wall9);
+   princess.collide(wall10);
+   princess.collide(wall11);
+   princess.collide(wall12);
+    princess.collide(wall13);
+    
+    princess.collide(wall15);
+    princess.collide(wall16);
+    princess.collide(wall17);
+    
+   princess.collide(wall18);
+   princess.collide(wall19);
+   princess.collide(wall20);
+   
+}
 
+function drawWalls4(){
+    wall21 = createSprite(260,550,90,10);
+    wall21.shapeColor="rgb(170,190,60)";
+    wall22 = createSprite(825,600,10,100);
+    wall22.shapeColor="rgb(170,190,60)";
+
+    princess.collide(wall);
+    princess.collide(wall1);
+    princess.collide(wall2);
+    princess.collide(wall3);
+    princess.collide(wall4);
+    princess.collide(wall5);
+    princess.collide(wall6);
+    princess.collide(wall7);
+    princess.collide(wall8);
+    princess.collide(wall9);
+    princess.collide(wall10);
+    princess.collide(wall11);
+    princess.collide(wall12);
+    princess.collide(wall13);
+    
+    princess.collide(wall15);
+    princess.collide(wall16);
+    princess.collide(wall17);
+    
+   princess.collide(wall18);
    princess.collide(wall19);
    princess.collide(wall20);
    princess.collide(wall21);
    princess.collide(wall22);
-   princess.collide(wall23);
 }
 
-function drawWalls4(){
 
-}
-
-function bouncing(){
-    
-    
-   
-}
